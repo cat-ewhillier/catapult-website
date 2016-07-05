8db8e8851759d69183ea71f17b0b8c61e66c5898
cat-ewhillier <ewhillier@catapultu.com>
2016-07-05 10:26:13 -0700

    CAROUSEL!!
    homepage carousel functionality

--- _/js/interactions.js
+++ _/js/interactions.js
@@ -1,7 +1,25 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     
     $('.button').click(function () {
         $('header').toggleClass('open');
+    });
+    
+    $('.nav-link-one').click(function () {
+        $('.story-one').removeClass('story-hide');
+        $('.story-two').addClass('story-hide');
+        $('.story-three').addClass('story-hide');
+    });
+    
+    $('.nav-link-two').click(function () {
+        $('.story-one').addClass('story-hide');
+        $('.story-two').removeClass('story-hide');
+        $('.story-three').addClass('story-hide');
+    });
+    
+    $('.nav-link-three').click(function () {
+        $('.story-one').addClass('story-hide');
+        $('.story-two').addClass('story-hide');
+        $('.story-three').removeClass('story-hide');
     });
     
     
