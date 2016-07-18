28084777b61f9f557e924d4dee8a50aaec2c1940
cat-ewhillier <ewhillier@catapultu.com>
2016-07-18 15:20:58 -0700

    added carousel navigation

--- _/js/interactions.js
+++ _/js/interactions.js
@@ -31,35 +31,61 @@ $(document).ready(function() {
         $('#carousel-button-three').addClass('carousel-selected');
     });
     
+    $('#next-one').click(function () {
+    	$('.story-three').addClass('story-hide');
+        $('.story-one').removeClass('story-hide');
+		$('#carousel-button-three').removeClass('carousel-selected');
+        $('#carousel-button-one').addClass('carousel-selected');
+    });
+    
+    $('#next-two').click(function () {
+    	$('.story-one').addClass('story-hide');
+        $('.story-two').removeClass('story-hide');
+		$('#carousel-button-one').removeClass('carousel-selected');
+        $('#carousel-button-two').addClass('carousel-selected');
+    });
+    
+    $('#next-three').click(function () {
+    	$('.story-two').addClass('story-hide');
+        $('.story-three').removeClass('story-hide');
+		$('#carousel-button-two').removeClass('carousel-selected');
+        $('#carousel-button-three').addClass('carousel-selected');
+    });
+    
+    $('#back-one').click(function () {
+    	$('.story-two').addClass('story-hide');
+        $('.story-one').removeClass('story-hide');
+		$('#carousel-button-two').removeClass('carousel-selected');
+        $('#carousel-button-one').addClass('carousel-selected');
+    });
+    
+    $('#back-two').click(function () {
+    	$('.story-three').addClass('story-hide');
+        $('.story-two').removeClass('story-hide');
+		$('#carousel-button-three').removeClass('carousel-selected');
+        $('#carousel-button-two').addClass('carousel-selected');
+    });
+    
+    $('#back-three').click(function () {
+    	$('.story-one').addClass('story-hide');
+        $('.story-three').removeClass('story-hide');
+		$('#carousel-button-one').removeClass('carousel-selected');
+        $('#carousel-button-three').addClass('carousel-selected');
+    });
+    
     
 
-  $('.container-2').hover(
-	
-       function () {
-          $('.work-filters').css("height", "80px");
-          $('#search').css("margin-bottom", "5px");
-       }, 
-		
-       function () {
-          $('.work-filters').css("height", "50px");
-          $('#search').css("margin-bottom", "0px");
-       }
-    );
-		
-		
-	$('.container-2').click(
-				
-	   function () {
-	      $('#dropdowns').addClass('hide');
-	   }, 
-		
-	   function () {
-	      $('#dropdowns').removeClass('hide');
-	   }
-	);
-			
-		
 
+	$('#harvest-snaps').hover(function () { 
+       $('.work-hero').css("backgroundImage", "url(_/assets/bsm-new-harvestsnaps.jpg)");  
+	});
+	
+	$('#maricopa-county').hover(function () { 
+       $('.work-hero').css("backgroundImage", "url(_/assets/Composed-Images-Vertical-CafeComp1.jpg)");  
+	});
+    
+    
+		
 
 });
     
